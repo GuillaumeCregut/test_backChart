f680e021142cfccfaccb67275380b766b1e71de0
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-05-08 11:29:48 +0200

    adding fake datas

---
+++ datas.js
@@ -0,0 +1,31 @@
+const datas=[
+    {
+        id:1,
+        value:15,
+        name: "A"
+    },
+    {
+        id:2,
+        value:39,
+        name:"B",
+    },
+    {
+        id:3,
+        value:72,
+        name: 'C'
+    },
+    {
+        id:4,
+        value:42,
+        name: 'D'
+    },
+    {
+        id:5,
+        value:155,
+        name:'E'
+    },
+]
+
+module.exports={
+    datas,
+}
