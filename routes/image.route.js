6d6e3b55706d0bb2b5dd1d10d4c3a36fe1aa2184
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-05-08 19:00:52 +0200

    sample generated

--- routes/image.route.js
+++ routes/image.route.js
@@ -1,7 +1,17 @@
 const router=require('express').Router();
 const {datas}=require('../datas');
-router.get('/',(req,res)=>{
+const {createImage}=require('../utils/generateLine');
+const {createPie}=require('../utils/generatePie');
+
+router.get('/bar',async (req,res)=>{
+    const img= await createImage();
+    console.log(img);
     res.json(datas);
 })
 
+router.get('/pie',async(req,res)=>{
+    const img= await createPie();
+    console.log(img);
+    res.json(datas);
+})
 module.exports=router;
