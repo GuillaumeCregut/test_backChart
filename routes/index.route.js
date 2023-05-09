fb8de7d38c507669fb7752513820416f149d60f8
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-05-09 19:04:46 +0200

    adding route for pdf

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,6 +1,8 @@
 const router=require('express').Router();
 const image=require('./image.route');
+const pdf=require('./pdf.route');
 
 router.use('/images',image);
+router.use('/pdf',pdf);
 
 module.exports = router;
