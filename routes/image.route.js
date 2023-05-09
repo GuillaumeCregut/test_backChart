c1c1ae4d8ada520d7b81d4070f7b74e0eb5a3804
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-05-09 18:49:49 +0200

    adding data connection to make pie

--- routes/image.route.js
+++ routes/image.route.js
@@ -2,6 +2,7 @@ const router=require('express').Router();
 const {datas}=require('../datas');
 const {createImage}=require('../utils/generateLine');
 const {createPie}=require('../utils/generatePie');
+const {getAll}=require('../models/model');
 
 router.get('/bar',async (req,res)=>{
     const img= await createImage();
@@ -9,12 +10,28 @@ router.get('/bar',async (req,res)=>{
     res.json(datas);
 })
 
+router.get('/piedb',async(req,res)=>{
+    const result=await getAll(5);
+    const labels=result.map((item)=>item.name);
+    const values=result.map((item)=>item.count);
+    try{
+        const img= await createPie(400,400,labels, values,'./example3.png');
+        res.json(result);
+    }
+    catch(err){
+        console.log(err);
+        res.sendStatus(500);
+    }
+    console.log(result);
+    
+})
+
 router.get('/pie',async(req,res)=>{
     const labels=datas.map((data)=>data.name);
     const dataContent=datas.map((data)=>data.value);
    // const datas=[150,300,252,42];
     try{
-        const img= await createPie(400,400,labels, dataContent,'./example3.png');
+        const img= await createPie(400,400,labels, dataContent,'./example2.png');
     }
     catch(err){
         console.log(err);
