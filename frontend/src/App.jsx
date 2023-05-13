0b71171b632f2d231de1898250153022b686aef4
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-05-13 15:25:26 +0200

    adding download pdf stream

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState('')
+  useEffect(()=>{
+    const getPDF=()=>{
+      const url='http://localhost:8000/api/pdf'
+      axios({url:url,method:'GET',responseType:'blob'})
+      .then((resp)=>{
+          const data=resp.data;
+          console.log(data)
+          const href=URL.createObjectURL(data);
+          console.log(href)
+          setCount(href);
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
+    }
+    getPDF();
+  },[]);
   return (
     <>
-    Hello
+    <a href={count}>toto</a>
+   hello
     </>
   )
 }
